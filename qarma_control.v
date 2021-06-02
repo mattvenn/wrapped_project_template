bef67ab2bda677d76fc047e07ac72baf67afc23c
pleb <you@example.com>
2021-06-01 19:51:02 -0700

    Minor fix

--- qarma_control.v
+++ qarma_control.v
@@ -8,6 +8,7 @@ module QarmaControl #(
     input wbs_stb_i,
     input wbs_cyc_i,
     input wbs_we_i,
+    input [3:0] wbs_sel_i,
     input [31:0] wbs_dat_i,
     input [31:0] wbs_adr_i,
     output wbs_ack_o,
