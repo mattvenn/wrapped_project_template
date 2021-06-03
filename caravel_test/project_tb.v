e007a5a6fb1b26a3428c1bef61d16c5ac9462454
ChrisDePalm <christopher.depalm@gmail.com>
2021-06-03 07:50:15 +0200

    Initial Commit

--- caravel_test/project_tb.v
+++ caravel_test/project_tb.v
@@ -1,5 +1,3 @@
-// SPDX-FileCopyrightText: 2020 Efabless Corporation
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -17,41 +15,87 @@
 
 `timescale 1 ns / 1 ps
 
-`include "uprj_netlists.v" // this file gets created automatically by multi_project_tools from the source section of info.yaml
+`include "uprj_netlists.v"
 `include "caravel_netlists.v"
 `include "spiflash.v"
 
-module project_tb;
-    initial begin
-        $dumpfile ("project.vcd");
-        $dumpvars (0, project_tb);
-        #1;
-    end
+module la_test2_tb;
+	reg clock;
+	reg RSTB;
+	reg CSB;
 
-	reg clk;
-    reg RSTB;
 	reg power1, power2;
-	reg power3, power4;
 
-    wire gpio;
-    wire [37:0] mprj_io;
+    	wire gpio;
+    	wire [37:0] mprj_io;
+	wire [15:0] checkbits;
 
-    ///// convenience signals that match what the cocotb test modules are looking for
+	assign checkbits = mprj_io[31:16];
+	assign mprj_io[3] = (CSB == 1'b1) ? 1'b1 : 1'bz;
 
+	always #12.5 clock <= (clock === 1'b0);
 
-    /////
+	initial begin
+		clock = 0;
+	end
 
+	initial begin
+		$dumpfile("project.vcd");
+		$dumpvars(0, la_test2_tb);
 
-	wire flash_csb;
+		// Repeat cycles of 1000 clock edges as needed to complete testbench
+		repeat (30) begin
+			repeat (1000) @(posedge clock);
+			$display("+1000 cycles");
+		end
+		$display("%c[1;31m",27);
+		`ifdef GL
+			$display ("Monitor: Timeout, Test Mega-Project IO (GL) Failed");
+		`else
+			$display ("Monitor: Timeout, Test Mega-Project IO (RTL) Failed");
+		`endif
+		$display("%c[0m",27);
+		$finish;
+	end
+/*
+	initial begin
+		wait(checkbits == 16'h AB60);
+		$display("Monitor: Test 2 MPRJ-Logic Analyzer Started");
+		wait(checkbits == 16'h AB61);
+		$display("Monitor: Test 2 MPRJ-Logic Analyzer Passed");
+		$finish;
+	end
+*/
+	initial begin
+		RSTB <= 1'b0;
+		CSB  <= 1'b1;		// Force CSB high
+		#2000;
+		RSTB <= 1'b1;	    	// Release reset
+		#170000;
+		CSB = 1'b0;		// CSB can be released
+	end
+
+	initial begin		// Power-up sequence
+		power1 <= 1'b0;
+		power2 <= 1'b0;
+		#200;
+		power1 <= 1'b1;
+		#200;
+		power2 <= 1'b1;
+	end
+
+    	wire flash_csb;
 	wire flash_clk;
 	wire flash_io0;
 	wire flash_io1;
 
-	wire VDD3V3 = power1;
-	wire VDD1V8 = power2;
-	wire USER_VDD3V3 = power3;
-	wire USER_VDD1V8 = power4;
-	wire VSS = 1'b0;
+	wire VDD1V8;
+    	wire VDD3V3;
+	wire VSS;
+    
+	assign VDD3V3 = power1;
+	assign VDD1V8 = power2;
+	assign VSS = 1'b0;
 
 	caravel uut (
 		.vddio	  (VDD3V3),
@@ -60,15 +104,15 @@ module project_tb;
 		.vssa	  (VSS),
 		.vccd	  (VDD1V8),
 		.vssd	  (VSS),
-		.vdda1    (USER_VDD3V3),
-		.vdda2    (USER_VDD3V3),
+		.vdda1    (VDD3V3),
+		.vdda2    (VDD3V3),
 		.vssa1	  (VSS),
 		.vssa2	  (VSS),
-		.vccd1	  (USER_VDD1V8),
-		.vccd2	  (USER_VDD1V8),
+		.vccd1	  (VDD1V8),
+		.vccd2	  (VDD1V8),
 		.vssd1	  (VSS),
 		.vssd2	  (VSS),
-		.clock	  (clk),
+		.clock	  (clock),
 		.gpio     (gpio),
         	.mprj_io  (mprj_io),
 		.flash_csb(flash_csb),
@@ -85,8 +129,8 @@ module project_tb;
 		.clk(flash_clk),
 		.io0(flash_io0),
 		.io1(flash_io1),
-		.io2(),			// not used
-		.io3()			// not used
+		.io2(),
+		.io3()
 	);
 
 endmodule
