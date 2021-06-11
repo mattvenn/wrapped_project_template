d2652a4367138b69edf1b3a0ccae5d9da202a5e8
pleb <you@example.com>
2021-06-11 16:52:18 -0700

    Fix: don't buffer output ports

--- config.tcl
+++ config.tcl
@@ -31,6 +31,8 @@ set ::env(GLB_RT_MAXLAYER) 5
 set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
 set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 
+set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
+
 # regular pin order seems to help with aggregating all the macros for the group project
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
