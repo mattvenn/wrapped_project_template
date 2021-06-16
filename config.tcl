c571c103499548146f1c9b3f95c58bc3764b06db
Matt Venn <matt@mattvenn.net>
2021-06-16 18:19:06 +0200

    reharden

--- config.tcl
+++ config.tcl
@@ -10,7 +10,7 @@ set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/wrapper.v \
     $::env(DESIGN_DIR)/qarma/qarma_64.v"
 
 # target density, change this if you can't get your design to fit
-set ::env(PL_TARGET_DENSITY) 0.52
+set ::env(PL_TARGET_DENSITY) 0.50
 
 # set absolute size of the die to 300 x 300 um
 set ::env(DIE_AREA) "0 0 350 350"
