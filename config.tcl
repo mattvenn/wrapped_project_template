35bad4464e4bcc45bb1cbfa9cb308338195268fe
Matt Venn <matt@mattvenn.net>
2021-12-08 13:51:28 +0100

    fix for mpw4

--- config.tcl
+++ config.tcl
@@ -13,14 +13,14 @@ set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/wrapper.v \
 set ::env(PL_TARGET_DENSITY) 0.50
 
 # set absolute size of the die to 300 x 300 um
-set ::env(DIE_AREA) "0 0 360 360"
+set ::env(DIE_AREA) "0 0 400 400"
 set ::env(FP_SIZING) absolute
 
 # define number of IO pads
 set ::env(SYNTH_DEFINES) "MPRJ_IO_PADS=38"
 
 # clock period is ns
-set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PERIOD) "25"
 set ::env(CLOCK_PORT) "wb_clk_i"
 
 # macro needs to work inside Caravel, so can't be core and can't use metal 5
@@ -28,8 +28,8 @@ set ::env(DESIGN_IS_CORE) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
 # define power straps so the macro works inside Caravel's PDN
-set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
-set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
 
 set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
 
@@ -38,3 +38,6 @@ set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 # turn off CVC as we have multiple power domains
 set ::env(RUN_CVC) 0
+
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.8
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.8
